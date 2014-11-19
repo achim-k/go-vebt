bcd23d064aaeeee4d14c0703d7f2327db0a7cd38
achimk1704 <achimk1704@gmail.com>
2014-11-19 20:24:25 +0900

    created basic structure + insert + isMember methods, simple testing

---
+++ vebt.go
@@ -0,0 +1,64 @@
+package vebt
+
+import (
+	//"fmt"
+	"math"
+)
+
+type VEB struct {
+    u, min, max int //universe size, min-, max value
+    summary *VEB 	//pointer to summary
+    cluster []*VEB 	// array of pointers to each child cluster
+}
+
+func (V VEB) Max() int { return V.max }
+func (V VEB) Min() int { return V.min }
+func (V VEB) High(x int) int { 
+	return int(math.Floor(float64(x)/float64(LowerSqrt(V.u))))
+}
+func (V VEB) Low(x int) int { 
+	return x % LowerSqrt(V.u)
+}
+func (V VEB) Index(x, y int) int { 
+	return x * LowerSqrt(V.u) + y
+}
+func (V VEB) IsMember(x int) bool {
+	if x == V.min || x == V.max {
+		return true
+	} else if V.u == 2 {
+        return false
+    } else {
+        return V.cluster[V.High(x)].IsMember(V.Low(x))
+    } 
+}
+func (V *VEB) Insert(x int) {
+	if V.min == -1 {
+		V.min, V.max = x, x
+	} else {
+		if x < V.min {
+			temp := V.min
+			V.min = x
+			x = temp
+		}
+		if V.u > 2 {
+			if V.cluster[V.High(x)].Min() == -1 {
+				V.summary.Insert(V.High(x))
+				V.cluster[V.High(x)].min, V.cluster[V.High(x)].max = V.Low(x), V.Low(x)
+			} else {
+				V.cluster[V.High(x)].Insert(V.Low(x))
+			}
+		}
+		if x > V.max {
+			V.max = x
+		}
+	}
+}
+
+// Calculate lower square root (helper function)
+func LowerSqrt(u int) int {
+	return int(math.Pow(2.0, math.Floor(math.Log2(float64(u)) / 2)))
+}
+// Calculate higher square root (helper function)
+func HigherSqrt(u int) int {
+	return int(math.Pow(2.0, math.Ceil(math.Log2(float64(u)) / 2)))
+}
---
+++ vebt_test.go
@@ -0,0 +1,115 @@
+package vebt
+
+import(
+	"testing"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func TestLowerSqrt(t *testing.T) {
+	const in, out = 32, 4
+	if x := LowerSqrt(in); x != out {
+		t.Errorf("LowerSqrt(%v) = %v, want %v", in, x, out)
+	}
+}
+
+func TestHigherSqrt(t *testing.T) {
+	const in, out = 32, 8
+	if x := HigherSqrt(in); x != out {
+		t.Errorf("HigherSqrt(%v) = %v, want %v", in, x, out)
+	}
+}
+
+func TestRun(t *testing.T) {
+	runs := 100
+
+	for i := 0; i < runs; i++ {
+
+		u := 16
+		rd := rand.New(rand.NewSource(int64(time.Now().Nanosecond())*2))
+		keys := []int{}
+		keyNo := rd.Intn(u)
+		//create random keys
+		for i := 0; i < keyNo; i++ {
+			keys = append(keys, rd.Intn(u - 1))
+		}
+
+		V := createTree(t)
+		insertMembershipTest(t, &V, keyNo, keys)
+	}
+}
+
+func createTree(t *testing.T) (VEB) {
+	const u = 16
+
+	// VEB(2) objects (layer3)
+	l2_0_0 := VEB{u: 2, min: -1, max: -1}
+	l2_0_1 := VEB{u: 2, min: -1, max: -1}
+	l2_1_0 := VEB{u: 2, min: -1, max: -1}
+	l2_1_1 := VEB{u: 2, min: -1, max: -1}
+	l2_2_0 := VEB{u: 2, min: -1, max: -1}
+	l2_2_1 := VEB{u: 2, min: -1, max: -1}
+	l2_3_0 := VEB{u: 2, min: -1, max: -1}
+	l2_3_1 := VEB{u: 2, min: -1, max: -1}
+
+	l2_0_s3 := VEB{u: 2, min: -1, max: -1}
+	l2_1_s3 := VEB{u: 2, min: -1, max: -1}
+	l2_2_s3 := VEB{u: 2, min: -1, max: -1}
+	l2_3_s3 := VEB{u: 2, min: -1, max: -1}
+
+	s2_0_s3_0 := VEB{u: 2, min: -1, max: -1}
+	s2_0_s3_1 := VEB{u: 2, min: -1, max: -1}
+	s2_0_s3_2 := VEB{u: 2, min: -1, max: -1}
+
+	
+	// VEB(4) objects
+	l2_0 := VEB{u: 4, min: -1, max: -1, summary: &l2_0_s3, cluster: []*VEB{&l2_0_0, &l2_0_1}}
+	l2_1 := VEB{u: 4, min: -1, max: -1, summary: &l2_1_s3, cluster: []*VEB{&l2_1_0, &l2_1_1}}
+	l2_2 := VEB{u: 4, min: -1, max: -1, summary: &l2_2_s3, cluster: []*VEB{&l2_2_0, &l2_2_1}}
+	l2_3 := VEB{u: 4, min: -1, max: -1, summary: &l2_3_s3, cluster: []*VEB{&l2_3_0, &l2_3_1}}
+	s2_0 := VEB{u: 4, min: -1, max: -1, summary: &s2_0_s3_0, cluster: []*VEB{&s2_0_s3_1, &s2_0_s3_2}}
+
+	// VEB(16)
+	l1_0 := VEB{u: 16, min: -1, max: -1, summary: &s2_0, cluster: []*VEB{&l2_0, &l2_1, &l2_2, &l2_3}}
+
+	return l1_0
+}
+
+func insertMembershipTest(t *testing.T, V *VEB, keyNo int, keys []int) {
+	//we expect an empty tree, test each key for emptiness first
+	for i := 0; i < V.u; i++ {
+		if V.IsMember(i) == true {
+			t.Errorf("IsMember(%v) = %v, want %v", i, true, false)
+		}
+	}
+
+	fmt.Printf("Random keys (%v): %v\n", keyNo, keys)
+
+	// Insert keys
+	for i := 0; i < len(keys); i++ {
+		V.Insert(keys[i])
+	}
+
+	// Member Check
+	for i := 0; i < V.u; i++ {
+		keyExists := false
+		for k := 0; k < len(keys); k++ {
+			if keys[k] == i {
+				keyExists = true	
+				break
+			}
+		}
+		// Check existing keys
+		if keyExists == true {
+			if V.IsMember(i) == false {
+				t.Errorf("IsMember(%v) = %v, want %v", i, false, true)
+			}	
+		} else { // CHeck nonexisting keys
+			if V.IsMember(i) == true {
+				t.Errorf("IsMember(%v) = %v, want %v", i, true, false)
+			}	
+		}
+	}
+
+}
